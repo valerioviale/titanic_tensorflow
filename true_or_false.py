8f2e908608dbd810922af58b5e913ab75f0539cc
viale.valerio@gmail.com <viale.valerio@gmail.com>
2023-10-29 08:38:34 +0100

    true or false

--- true_or_false.py
+++ true_or_false.py
@@ -1,1 +1,16 @@
+# 1. When we set `a = 256` and `b = 256`, they both refer to the same memory 
+# location because 256 falls within the range of cached small integers. Therefore, 
+#`a is b` returns `True`.
+
+a = 256
+b = 256
+a is b
+ 
+
+# 2. When we set `a = 257` and `b = 257`, they are no longer within the range of cached 
+# small integers, so they are not interned, and `a is b` returns `False`.
+
+
+a = 257
+b = 257
 a is b
