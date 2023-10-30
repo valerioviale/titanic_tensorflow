ed0accd842a1f51695e0f866f67d00b5a4ab83a6
viale.valerio@gmail.com <viale.valerio@gmail.com>
2023-10-30 16:35:30 +0100

    improvement to the model

--- titanic_bard.py
+++ titanic_bard.py
@@ -1,76 +1,203 @@
-import optuna
+import numpy as np
+import pandas as pd
+import os
+
 import tensorflow as tf
 import tensorflow_decision_forests as tfdf
-import pandas as pd
+
+print(f"Found TF-DF {tfdf.__version__}")
+
+train_df = pd.read_csv("train.csv")
+serving_df = pd.read_csv("test.csv")
+
+train_df.head(10)
+
 
 def preprocess(df):
-    # TODO: Implement your preprocessing steps here.
+    df = df.copy()
+
+    def normalize_name(x):
+        return " ".join([v.strip(",()[].\"'") for v in x.split(" ")])
+
+    def ticket_number(x):
+        return x.split(" ")[-1]
+
+    def ticket_item(x):
+        items = x.split(" ")
+        if len(items) == 1:
+            return "NONE"
+        return "_".join(items[0:-1])
+
+    df["Name"] = df["Name"].apply(normalize_name)
+    df["Ticket_number"] = df["Ticket"].apply(ticket_number)
+    df["Ticket_item"] = df["Ticket"].apply(ticket_item)
     return df
 
-def train_model(train_ds, hyperparameters):
-    model = tfdf.keras.GradientBoostedTreesModel(**hyperparameters)
+
+preprocessed_train_df = preprocess(train_df)
+preprocessed_serving_df = preprocess(serving_df)
+
+preprocessed_train_df.head(5)
+
+
+input_features = list(preprocessed_train_df.columns)
+input_features.remove("Ticket")
+input_features.remove("PassengerId")
+input_features.remove("Survived")
+# input_features.remove("Ticket_number")
+
+print(f"Input features: {input_features}")
+
+
+def tokenize_names(features, labels=None):
+    """Divite the names into tokens. TF-DF can consume text tokens natively."""
+    features["Name"] = tf.strings.split(features["Name"])
+    return features, labels
+
+
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(
+    preprocessed_train_df, label="Survived").map(tokenize_names)
+serving_ds = tfdf.keras.pd_dataframe_to_tf_dataset(
+    preprocessed_serving_df).map(tokenize_names)
+
+
+model = tfdf.keras.GradientBoostedTreesModel(
+    verbose=0,  # Very few logs
+    features=[tfdf.keras.FeatureUsage(name=n) for n in input_features],
+    exclude_non_specified_features=True,  # Only use the features in "features"
+
+    # num_trees=2000,
+
+    # Only for GBT.
+    # A bit slower, but great to understand the model.
+    # compute_permutation_variable_importance=True,
+
+    # Change the default hyper-parameters
+    # hyperparameter_template="benchmark_rank1@v1",
+
+    # num_trees=1000,
+    # tuner=tuner
+
+    min_examples=1,
+    categorical_algorithm="RANDOM",
+    # max_depth=4,
+    shrinkage=0.05,
+    # num_candidate_attributes_ratio=0.2,
+    split_axis="SPARSE_OBLIQUE",
+    sparse_oblique_normalization="MIN_MAX",
+    sparse_oblique_num_projections_exponent=2.0,
+    # num_trees=2000,
+    # validation_ratio=0.0,
+    random_seed=1234,
+
+)
+model.fit(train_ds)
+
+
+self_evaluation = model.make_inspector().evaluation()
+print(f"Accuracy: {self_evaluation.accuracy} Loss:{self_evaluation.loss}")
+
+
+model.summary()
+
+
+def prediction_to_kaggle_format(model, threshold=0.5):
+    proba_survive = model.predict(serving_ds, verbose=0)[:, 0]
+    return pd.DataFrame({
+        "PassengerId": serving_df["PassengerId"],
+        "Survived": (proba_survive >= threshold).astype(int)
+    })
+
+
+def make_submission(kaggle_predictions):
+    path = "submission.csv"
+    kaggle_predictions.to_csv(path, index=False)
+    print(f"Submission exported to {path}")
+    with open(path, "r") as file:
+        contents = file.read()
+    print(contents)
+
+
+
+kaggle_predictions = prediction_to_kaggle_format(model)
+make_submission(kaggle_predictions)
+# Read and display the contents of the "submission.csv" file using Python
+with open("submission.csv", "r") as file:
+    contents = file.read()
+
+print(contents)
+
+tuner = tfdf.tuner.RandomSearch(num_trials=1000)
+tuner.choice("min_examples", [2, 5, 7, 10])
+tuner.choice("categorical_algorithm", ["CART", "RANDOM"])
+
+local_search_space = tuner.choice("growing_strategy", ["LOCAL"])
+local_search_space.choice("max_depth", [3, 4, 5, 6, 8])
+
+global_search_space = tuner.choice(
+    "growing_strategy", ["BEST_FIRST_GLOBAL"], merge=True)
+global_search_space.choice("max_num_nodes", [16, 32, 64, 128, 256])
+
+#tuner.choice("use_hessian_gain", [True, False])
+tuner.choice("shrinkage", [0.02, 0.05, 0.10, 0.15])
+tuner.choice("num_candidate_attributes_ratio", [0.2, 0.5, 0.9, 1.0])
+
+
+tuner.choice("split_axis", ["AXIS_ALIGNED"])
+oblique_space = tuner.choice("split_axis", ["SPARSE_OBLIQUE"], merge=True)
+oblique_space.choice("sparse_oblique_normalization",
+                     ["NONE", "STANDARD_DEVIATION", "MIN_MAX"])
+oblique_space.choice("sparse_oblique_weights", ["BINARY", "CONTINUOUS"])
+oblique_space.choice("sparse_oblique_num_projections_exponent", [1.0, 1.5])
+
+# Tune the model. Notice the `tuner=tuner`.
+tuned_model = tfdf.keras.GradientBoostedTreesModel(tuner=tuner)
+tuned_model.fit(train_ds, verbose=0)
+
+tuned_self_evaluation = tuned_model.make_inspector().evaluation()
+print(
+    f"Accuracy: {tuned_self_evaluation.accuracy} Loss:{tuned_self_evaluation.loss}")
+
+
+predictions = None
+num_predictions = 0
+
+for i in range(100):
+    print(f"i:{i}")
+    # Possible models: GradientBoostedTreesModel or RandomForestModel
+    model = tfdf.keras.GradientBoostedTreesModel(
+        verbose=0,  # Very few logs
+        features=[tfdf.keras.FeatureUsage(name=n) for n in input_features],
+        exclude_non_specified_features=True,  # Only use the features in "features"
+
+        # min_examples=1,
+        # categorical_algorithm="RANDOM",
+        # max_depth=4,
+        # shrinkage=0.05,
+        # num_candidate_attributes_ratio=0.2,
+        # split_axis="SPARSE_OBLIQUE",
+        # sparse_oblique_normalization="MIN_MAX",
+        # sparse_oblique_num_projections_exponent=2.0,
+        # num_trees=2000,
+        # validation_ratio=0.0,
+        random_seed=i,
+        honest=True,
+    )
     model.fit(train_ds)
-    return model
 
-def evaluate_model(model, test_ds):
-    predictions = model.predict(test_ds, verbose=0)[:, 0]
-    accuracy = (predictions >= 0.5).astype(int) == test_ds["Survived"]
-    return accuracy.mean()
+    sub_predictions = model.predict(serving_ds, verbose=0)[:, 0]
+    if predictions is None:
+        predictions = sub_predictions
+    else:
+        predictions += sub_predictions
+    num_predictions += 1
 
-def tune_hyperparameters(train_ds, test_ds):
-    def objective(trial):
-        trial.suggest_int("num_trees", 100, 1000, log=True)
-        trial.suggest_int("max_depth", 3, 8, log=True)
-        trial.suggest_float("shrinkage", 0.01, 0.1)
+predictions /= num_predictions
 
-        model = train_model(train_ds, trial.params)
-        accuracy = evaluate_model(model, test_ds)
-        return accuracy
+kaggle_predictions = pd.DataFrame({
+    "PassengerId": serving_df["PassengerId"],
+    "Survived": (predictions >= 0.5).astype(int)
+})
 
-    study = optuna.create_study()
-    study.optimize(objective, n_trials=100)
+make_submission(kaggle_predictions)
 
-    return study.best_trial.params
-
-def main():
-    try:
- # Load the data, pre-process it, tune the hyperparameters, train the model, evaluate the model, and write the results to submission.csv.
-        ...
-    except Exception as e:
-        print(e)
-    # Load the data.
-    train_df = pd.read_csv("train.csv")
-    test_df = pd.read_csv("test.csv")
-
-    # Preprocess the data.
-    train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(preprocess(train_df))
-    test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(preprocess(test_df))
-
-    # Tune the hyperparameters.
-    hyperparameters = tune_hyperparameters(train_ds, test_ds)
-
-    # Train the model with the best hyperparameters.
-    model = train_model(train_ds, hyperparameters)
-
-    # Evaluate the model on the test set.
-    accuracy = evaluate_model(model, test_ds)
-    print("Accuracy:", accuracy)
-    
-    # Write results to submission.csv
-    submission_df = pd.DataFrame({
-        "PassengerId": test_df["PassengerId"],
-        "Survived": (model.predict(test_ds, verbose=0)[:, 0] >= 0.5).astype(int)
-    })
-    submission_df.to_csv("submission.csv", index=False)
-    print("Results written to submission.csv")
-
-if __name__ == "__main__":
-    main()
-    
-    # Write results to submission.csv
-    submission_df = pd.DataFrame({
-        "PassengerId": test_df["PassengerId"],
-        "Survived": (model.predict(test_ds, verbose=0)[:, 0] >= 0.5).astype(int)
-    })
-    submission_df.to_csv("submission.csv", index=False)
-    print("Results written to submission.csv")
